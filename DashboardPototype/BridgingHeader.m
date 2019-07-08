f410706351e6b178fc8e5efcc0e885fe32672c44
Supanat Wanroj <supanat.wanroj@scb.co.th>
2019-07-08 17:20:14 +0700

    Revert "no message"
    This reverts commit c372c93af7bae97c539fd060dfbc035ef8a46604.

--- DashboardPototype/BridgingHeader.m
+++
@@ -1,9 +0,0 @@
-//
-//  BridgingHeader.m
-//  DashboardPototype
-//
-//  Created by Nutcha Jirum on 5/7/2562 BE.
-//  Copyright © 2562 Supanat Wanroj. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
