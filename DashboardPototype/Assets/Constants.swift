66cbaf89be4b09f8ecc1916b162be800d6eed9ff
nutcha.jirum <nutcha.jirum@scb.co.th>
2019-07-17 17:32:26 +0700

    EDIT MODEL AND EDIT REFRESHTOKEN

--- DashboardPototype/Assets/Constants.swift
+++ DashboardPototype/Assets/Constants.swift
@@ -13,5 +13,6 @@ class Constants {
     static let mostDoExamServiceUrl = "http://localhost:9000/api/exam/exam_most"
     static let recentExamServiceUrl = "http://localhost:9000/api/exam/last_exam/1"
     static let historyServiceUrl = "http://localhost:9000/e-xam/api/exam/history"
-    static let loginServiceUrl = "http://192.168.43.112:9988/api/auth"
+    static let loginServiceUrl = "http://localhost:8083/api/auth"
+    static let refreshTokenServiceUrl = "http://localhost:8083/api/auth/regen"
 }
